8e8ea9423708539d599bac9dbb6c4ea4a5f8400d
Teddi Maull <teddi@picturetheday.com>
2015-06-05 18:20:27 -0600

    initial commit

---
+++ build/.module-cache/1f8a5144130deaded396fde08bfe00c9acf65b72.js
@@ -0,0 +1,9 @@
+React.render(
+  React.createElement("h1", null, "Hello, world!"),
+  document.getElementById('example')
+);
+
+React.render(
+  React.createElement('h1', null, 'Hello, world!'),
+  document.getElementById('example')
+);
